503cbf652eb964c5e305133190556d4d17d4f665
Newton <newtoljunior@hotmail.com>
2020-03-11 18:49:32 -0300

    biblioteca para carregamento de imagem e alteracao no carregamento da logo na splash

--- appMax/app/src/main/java/br/com/newton/appmax/SplashActivity.kt
+++ appMax/app/src/main/java/br/com/newton/appmax/SplashActivity.kt
@@ -2,11 +2,23 @@ package br.com.newton.appmax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Toast
+import br.com.redcode.easyglide.library.load
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        imageViewLogo.load(R.drawable.maxima_logotipo)
+
+        val handle = Handler()
+        handle.postDelayed(Runnable {
+
+        }, 2000)
+
     }
 }
