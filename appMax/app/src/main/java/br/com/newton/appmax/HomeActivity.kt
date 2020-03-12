cf13bbdbf8afeaa43cbb062665999210bc41190f
Newton <newtoljunior@hotmail.com>
2020-03-11 21:31:42 -0300

    criada a tela principal

--- appMax/app/src/main/java/br/com/newton/appmax/HomeActivity.kt
+++ appMax/app/src/main/java/br/com/newton/appmax/HomeActivity.kt
@@ -2,11 +2,19 @@ package br.com.newton.appmax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.redcode.easyglide.library.load
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        imageViewClientes.load(R.drawable.ic_maxima_pessoas)
+        imageViewPedidos.load(R.drawable.ic_maxima_pedido)
+        imageViewResumo.load(R.drawable.ic_maxima_resumo_vendas)
+        imageViewFerramentas.load(R.drawable.ic_maxima_ferramentas)
+
     }
 }
