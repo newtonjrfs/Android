162f6ff182fd810b3d39314f49d718f6ebbeed8d
Newton <newtoljunior@hotmail.com>
2020-03-14 11:54:47 -0300

    ajustando a questao de nao ser nulavel da variavel

--- appMax/app/src/main/java/br/com/newton/appmax/model/request/Cliente.kt
+++ appMax/app/src/main/java/br/com/newton/appmax/model/request/Cliente.kt
@@ -3,7 +3,7 @@ package br.com.newton.appmax.model.request
 data class Cliente(
     val cnpj: String?, // 10.766.206/002-61
     val codigo: String?, // 40795A
-    val contatos: List<Contato?>?,
+    val contatos: List<Contato>?,
     val endereco: String?, // Avenida 136 - Setor Sul - Goiânia/Goiás
     val id: Int?, // 30987
     val nomeFantasia: String?, // Máxima Sistemas
