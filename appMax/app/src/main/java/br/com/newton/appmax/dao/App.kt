6067e1d013f7fbb677ee6af996c467d1a4e633d3
Newton <newtoljunior@hotmail.com>
2020-03-16 09:45:15 -0300

    criado o servico de notificacao

--- appMax/app/src/main/java/br/com/newton/appmax/dao/App.kt
+++ appMax/app/src/main/java/br/com/newton/appmax/dao/App.kt
@@ -2,9 +2,15 @@ package br.com.newton.appmax.dao
 
 import android.app.Application
 import android.content.Context
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
+import br.com.newton.appmax.general.SyncWorker
 import java.lang.ref.WeakReference
+import java.util.concurrent.TimeUnit
 
 class App : Application() {
+
+    var statusApp = false
 
     companion object {
         private lateinit var mContext: WeakReference<Context>
@@ -14,6 +20,21 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         mContext = WeakReference(this)
+        statusApp = true
+
+        val jobSyncWorker = PeriodicWorkRequest.Builder(
+            SyncWorker::class.java,
+            5,
+            TimeUnit.MINUTES,
+            1,
+            TimeUnit.MILLISECONDS
+        ).build()
+        WorkManager.getInstance(this).enqueue(jobSyncWorker)
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        statusApp = false
     }
 
 
---
+++ appMax/app/src/main/java/br/com/newton/appmax/general/WorkManager.kt
@@ -0,0 +1,74 @@
+package br.com.newton.appmax.general
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import br.com.newton.appmax.R
+import br.com.newton.appmax.dao.App
+import br.com.newton.appmax.view.activity.SplashActivity
+
+class SyncWorker(appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams) {
+
+    val app by lazy { App() }
+
+    override fun doWork(): Result {
+
+        startNotification()
+
+        return Result.success()
+    }
+
+    private fun startNotification() {
+
+        if (!app.statusApp) {
+
+            createNotificationChannel(applicationContext)
+
+            val intent = Intent(applicationContext, SplashActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent =
+                PendingIntent.getActivity(applicationContext, 0, intent, 0)
+
+            val builder = NotificationCompat.Builder(applicationContext, "channel_notification")
+                .setSmallIcon(R.drawable.maxima_logotipo)
+                .setContentTitle("Volte ao aplicativo")
+                .setContentText("Volte a usar o aplicativo da Maxima")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+            with(NotificationManagerCompat.from(applicationContext)) {
+                notify(2000, builder.build())
+            }
+        }
+
+    }
+
+    private fun createNotificationChannel(context: Context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channel_notification"
+            val descriptionText = "channel_app"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("channel_notification", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                context.getSystemService(NotificationManager::class.java)!!
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+}
